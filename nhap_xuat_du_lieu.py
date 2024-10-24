8586010818c43035a249435a27405bcecb425040
Huyen18A2 <ptthuyen.24174600092@sv.uneti.edu.vn>
2024-10-24 11:20:56 +0700

    xoa nhap xuat du lieu

--- nhap_xuat_du_lieu.py
+++
@@ -1,7 +0,0 @@
-a = float(input("nhap vao so a: "))
-b = float(input("nhap vao so b: "))
-c = float(input("nhap vao so c: "))
-trung_binh_cong = (a + b + c) / 3
-print(f"trung binh cong cua 3 so {a}, {b}, {c} la {trung_binh_cong:.3f}")
-
-
