e68f66b537156f97579dc8aebb01479f0e871396
Huyen18A2 <ptthuyen.24174600092@sv.uneti.edu.vn>
2024-10-24 23:53:49 +0700

    btap 8b

--- btap_8.py
+++ btap_8.py
@@ -2,6 +2,7 @@ import math
 x = float(input("nhap gia tri cua x: "))
 if x > 0 and x != 1:
     f_x = math.log(x, 4) + math.log(2, x)
+    f_x_rounded = round(f_x, 2)
     print(f"gia tri can tim f(x) = {f_x:.2f}")
 else:
     print("gia tri x khong thoa man")
